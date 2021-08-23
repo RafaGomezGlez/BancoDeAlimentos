ffdd91a81937b51e67334eed88a60b48a9ca65a6
RafaGomezGlez <A01733983@itesm.mx>
2021-08-23 18:17:19 -0500

    Initial files

---
+++ App/app/src/main/java/tec/mx/bancodecomida/Milestones.kt
@@ -0,0 +1,11 @@
+package tec.mx.bancodecomida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Milestones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_milestones)
+    }
+}
